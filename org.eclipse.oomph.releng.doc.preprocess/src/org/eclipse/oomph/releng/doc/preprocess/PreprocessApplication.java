4778711488aa28520282fc376f6c5ef3adf5bc8d
Ed Merks <ed.merks@gmail.com>
2014-09-23 09:32:56 +0200

    Add preprecessor support.

---
+++ org.eclipse.oomph.releng.doc.preprocess/src/org/eclipse/oomph/releng/doc/preprocess/PreprocessApplication.java
@@ -0,0 +1,18 @@
+package org.eclipse.oomph.releng.doc.preprocess;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+public class PreprocessApplication implements IApplication
+{
+
+  public Object start(IApplicationContext context) throws Exception
+  {
+    return null;
+  }
+
+  public void stop()
+  {
+  }
+
+}
