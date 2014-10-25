36a918e5d6c660f018d61ae49aa9d9ea57cb9bde
Ed Merks <ed.merks@gmail.com>
2014-10-25 16:48:15 +0200

    Support forcing p2 tasks to be enabled for preprocessing.

--- org.eclipse.oomph.releng.doc/src/org/eclipse/oomph/releng/doc/DocDash.java
+++ org.eclipse.oomph.releng.doc/src/org/eclipse/oomph/releng/doc/DocDash.java
@@ -727,7 +727,7 @@ public class DocDash extends ViewPart
     wc.setAttribute("org.eclipse.jdt.launching.PROGRAM_ARGUMENTS", "-os ${target.os} -ws ${target.ws} -arch ${target.arch} -nl ${target.nl} -consoleLog");
     wc.setAttribute("org.eclipse.jdt.launching.SOURCE_PATH_PROVIDER", "org.eclipse.pde.ui.workbenchClasspathProvider");
     wc.setAttribute("org.eclipse.jdt.launching.VM_ARGUMENTS",
-        "-Xms1024m -Xmx2500m -XX:MaxPermSize=512m -Doomph.setup.skip=true -Dpreprocessor.workspace=${workspace_loc} -Dpreprocessor.projects="
+        "-Xms1024m -Xmx2500m -XX:MaxPermSize=512m -Doomph.setup.skip=true -Doomph.setup.force=true -Dpreprocessor.workspace=${workspace_loc} -Dpreprocessor.projects="
             + preprocessorProjects);
     wc.setAttribute("pde.version", "3.3");
     wc.setAttribute("product", "org.eclipse.sdk.ide");
